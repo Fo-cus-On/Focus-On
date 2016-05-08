b94fed605cb2301231e89ccba1467099713bfd19
TheQueenEileen <elaisses@163.com>
2016-05-09 01:26:20 +0800

    Update and rename update1.php to update2.php

---
+++ update2.php
@@ -0,0 +1,14 @@
+<?php
+require_once('./conn.php');
+
+$sql = "select * from `$_POST[database]` order by id desc limit 1,1";
+$query = mysqli_query($connect,$sql);
+$list = mysqli_fetch_array($query);
+$result0 = array("newstitle" => $list['title'],"newsurl" => $list['url']);
+
+foreach($result0 as $key => $value){
+	$result0[$key] = urlencode($value);
+	}
+	echo $result = urldecode(json_encode($result0));
+
+?>
